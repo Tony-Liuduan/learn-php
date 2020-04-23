b7ca7cfe503cf2a06ed471dac7c4e70c5596770a
liuduan <liuduan@58.com>
2020-04-23 20:43:30 +0800

    update db config

--- basic/config/db.php
+++ basic/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=db_yii2basic',
     'username' => 'root',
-    'password' => '898989',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
